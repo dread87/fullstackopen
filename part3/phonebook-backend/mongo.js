291ab5058edb48596f2f1d00ac81fd0080d5fd3d
Dread <dread97@proton.me>
2023-03-23 23:47:38 +0200

    Add exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length !== 5 && process.argv.length !== 3) {
+    console.log(process.argv.length)
+    console.log('Invalid options', "Try 'node mongo.js [password] [name] [number]");
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://dread97:${password}@dev.lhtnvio.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+    const person = new Person({
+        name, number
+    })
+
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+}
+else {
+    console.log("phonebook:");
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
