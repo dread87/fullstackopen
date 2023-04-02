59b117d186cb4a36bd7954242e10b4b2845031d4
Dread <dread97@proton.me>
2023-04-02 04:25:30 +0300

    Add ex 4.5-4.7

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -8,4 +10,48 @@ const totalLikes = (blogs) => {
         : blogs.reduce((likeCount, blog) => likeCount + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    return blogs.reduce((mostLikedBlog, blog) => {
+        if (blog.likes > mostLikedBlog.likes) {
+            return blog
+        }
+        else {
+            return mostLikedBlog
+        }
+    }, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+    if(blogs.length===0){
+        return null
+    }
+
+    const counts = _.countBy(blogs, 'author');
+    const mostFrequent = _.maxBy(_.keys(counts), (author) => counts[author])
+
+    return {
+        author: mostFrequent,
+        blogs: counts[mostFrequent]
+    }
+}
+
+const mostLikes = (blogs)=>{
+    if(blogs.length===0){
+        return null
+    }
+
+    const grouped = _.groupBy(blogs,'author');
+    const sums = _.mapValues(grouped,(items)=>_.sumBy(items,'likes'))
+    const authorWithMostLikes = _.maxBy(_.keys(sums),(author)=>sums[author])
+
+    return {
+        author: authorWithMostLikes,
+        likes: sums[authorWithMostLikes]
+    }
+
+}
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
